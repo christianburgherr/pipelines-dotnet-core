e49edf1bddf26de4f9c090a2d31dc08b62193779
christian.burgherr <christian.burgherr@noser.com>
2021-01-13 11:51:30 +0100

    Solution: Introduce subfolder src and test project

---
+++ tests/pipelines-dotnet-core-test/UnitTestBase.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace pipelines_dotnet_core_test
+{
+    [TestClass]
+    public class UnitTestBase
+    {
+        [TestMethod]
+        public void TestMethodInitial()
+        {
+        }
+    }
+}
